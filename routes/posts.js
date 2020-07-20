97fcc290efc24dea9b2d980ae7e0d448366d3310
yuehhan <yuehwhan@gmail.com>
2020-07-20 01:22:53 -0400

    moved file to correct place

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('./models/Post');
+const Post = require('../models/Post');
 
 //Get all posts
 router.get('/', async (req, res) =>{
